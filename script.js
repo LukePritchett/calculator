fffaaa9d940fa675781bfa295bcd656ff06ab954
Luke Pritchett <robert.luke.pritchett@gmail.com>
2019-11-05 15:48:27 +1300

    fix issue with division

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ class Calculator {
             case '*':
                 computation = prev * current
                 break
-            case '/':
+            case '÷':
                 computation = prev / current
                 break
             default:
